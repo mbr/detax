c19dd4855f79e56691f106930c82b51b761da9f1
Marc Brinkmann <git@marcbrinkmann.de>
2016-08-28 11:33:26 +0200

    Added initial implementation.

---
+++ detax.py
@@ -0,0 +1,59 @@
+from math import floor
+
+
+class TaxData(object):
+    def __init__(self, year):
+        self.year = year
+        self.income = 0
+        self.children = []
+        self.tax_class = 1
+
+    def calc_tax(self):
+        if self.children:
+            raise NotImplementedError
+
+        if self.year not in (2015, 2016, 2017):
+            raise NotImplementedError
+
+        if self.tax_class != 1:
+            raise NotImplementedError
+
+        taxes = []
+
+        # Grundfreibetrag
+        if self.income <= 8652:
+            taxes.append((0, 'Grundfreibetrag'))
+        elif self.income <= 13669:
+            y = (self.income - 8652) / 10000.0
+            t = floor((993.62 * y + 1400) * y)
+            taxes.append((t, 'Einkommenssteuer'))
+        elif self.income <= 53665:
+            z = (self.income - 13669) / 10000.0
+            t = floor((225.4 * z + 2397) * z + 952.48)
+            taxes.append((t, 'Einkommenssteuer'))
+        elif self.income <= 254446:
+            t = floor(0.42 * self.income - 8394.14)
+            taxes.append((t, 'Einkommenssteuer'))
+        else:
+            t = floor(0.45 * self.income - 16027.52)
+            taxes.append((t, 'Einkommenssteuer'))
+
+        if self.tax_class == 1:
+            if taxes[0][0] > 972:
+                # Solidaritätszuschlag
+                soli = round(taxes[0][0] * 0.055, 2)
+                taxes.append((soli, 'Solidaritätszuschlag'))
+        else:
+            raise NotImplementedError
+
+        return taxes
+
+
+if __name__ == '__main__':
+    d = TaxData(2016)
+    d.income = 25000
+
+    tax = d.calc_tax()
+
+    print(tax)
+    print('Total: {:.2f} EUR'.format(sum(e[0] for e in tax)))
